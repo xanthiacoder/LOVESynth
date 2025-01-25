d96478f66f2432a85220e9cdc4b2da5de63433f7
Joash Chee <joash@scorelessmusic.com>
2025-01-25 11:37:07 +0800

    Started testing for R36S / ArkOS compatibility

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # LOVESynth
-A fluidsynth binding for LOVE2D.
+A fluidsynth binding for LOVE2D, supporting Windows, Mac, Linux and the R36S game console. (R36S running ArkOS, still a work in progress due to pathing issues)
 
 # Credits
-Original code by Virus
-Additional code by Poltergasm
-Patching by Xanthia
+* Original code by Virus
+* Additional code by Poltergasm
+* Patching by Xanthia
+* Playing music using soundfonts by https://github.com/FluidSynth/fluidsynth
--- main.lua
+++ main.lua
@@ -1,18 +1,39 @@
+-- for ArkOS file system compatibility, tested on ArkOS 11272024 version 
+love.filesystem.setIdentity("LOVESynth") 
 
--- set up the save directory
-love.filesystem.createDirectory("soundfonts")
-love.filesystem.createDirectory("saves")
-love.filesystem.createDirectory("exports")
+
 
 local function file_exists(file)
 	return love.filesystem.getInfo(file)
 end
 
+-- Set the platform the app is running on
 local platform = love.system.getOS()
 
 if platform ~= "Linux" and platform ~= "Windows" and platform ~= "OS X" then
-	error("Lovebase will not run on " .. platform)
+	error("LOVESynth will not run on " .. platform)
 end
+
+
+-- check for ArkOS system
+system = "" -- making global since global awareness required
+
+if love.filesystem.getUserDirectory( ) == "/home/ark/" then
+	system = "ArkOS"
+	-- set up the save directory using LUA I/O to write
+	os.execute("mkdir " .. love.filesystem.getSaveDirectory()) -- OS creation
+	os.execute("mkdir " .. love.filesystem.getSaveDirectory() .. "//soundfonts")
+	os.execute("mkdir " .. love.filesystem.getSaveDirectory() .. "//saves")
+	os.execute("mkdir " .. love.filesystem.getSaveDirectory() .. "//exports")
+else
+	system = "Others"
+	-- set up the save directory using Love FS (win, mac, linux)
+	love.filesystem.createDirectory("soundfonts")
+	love.filesystem.createDirectory("saves")
+	love.filesystem.createDirectory("exports")
+end
+
+
 
 -- the default soundfont to load
 local default_sf = "example.sf2"
@@ -21,7 +42,13 @@ local default_sf = "example.sf2"
 -- files and store it there
 if not file_exists("soundfonts/" .. default_sf) then
 	local data = love.filesystem.read("files/" .. default_sf)
-	love.filesystem.write("soundfonts/" .. default_sf, data)
+	if system == "Others" then
+		love.filesystem.write("soundfonts/" .. default_sf, data)
+	elseif system == "ArkOS" then
+		local file = io.open(love.filesystem.getSaveDirectory().."//soundfonts/"  .. default_sf, "w+")
+		file:write(data)
+		file:close()
+	end
 	data = nil
 end
 
@@ -54,7 +81,14 @@ if not file_exists(libfile) then
 		end
 	else
 		local libdata = love.filesystem.read("files/" .. libfile)
+		-- writing file for OS X and Linux
 		love.filesystem.write(libfile, libdata)
+		-- additional code for ArkOS since only LUA I/O works for it
+		if system == "ArkOS" then
+			local file = io.open(love.filesystem.getSaveDirectory().."//"  .. libfile, "w+")
+			file:write(libdata)
+			file:close()
+		end
 		libdata = nil
 	end
 end
